28ce9116ff732ae87a86c25398512907b66b4dea
Jonathan Cruz Rocha <joohncruzrocha@gmail.com>
2020-10-27 10:00:54 -0300

    study: listGuesser

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    {/* O list={ListGuesser} prop significa que react-admin deve usar o <ListGuesser>
+    componente para exibir a lista de postagens. Este componente adivinha o 
+    formato a ser usado para as colunas da lista com base nos dados buscados na API*/}
     <Resource name="users" list={ListGuesser} />
   </Admin>
 );
