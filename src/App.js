79d8a6fb7ab1b5b2a49281485f78fb7d70066619
Jonathan Cruz Rocha <joohncruzrocha@gmail.com>
2020-10-29 14:50:57 -0300

    feat: menu icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import * as React from "react";
 import { Admin, Resource } from 'react-admin';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+
 import { UserList } from './users';
 import { PostList } from './posts';
 import { PostEdit } from './postEdit';
@@ -10,8 +13,8 @@ import jsonServerProvider from 'ra-data-json-server';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-    <Resource name="users" list={UserList} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+    <Resource name="users" list={UserList} icon={UserIcon}/>
   </Admin>
 );
 
